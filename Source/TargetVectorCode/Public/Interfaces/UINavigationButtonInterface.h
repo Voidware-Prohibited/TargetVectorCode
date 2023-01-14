2c0521f1005797ce164455be19543181a5e56674
colorindarkness <urielballinas@gmail.com>
2023-01-13 23:11:46 -0800

    Renamed UINavigationTabInterface to UINavigationButtonInterface

---
+++ Source/TargetVectorCode/Public/Interfaces/UINavigationButtonInterface.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "Widgets/CommonActivatableWidgetContainer.h"
+#include "CommonActivatableWidget.h"
+#include "CommonButtonBase.h"
+#include "UINavigationButtonInterface.generated.h"
+
+UINTERFACE(Blueprintable)
+class UUINavigationButtonInterface : public UInterface {
+	GENERATED_BODY()
+};
+
+class TARGETVECTORCODE_API IUINavigationButtonInterface {
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Navigation Button")
+	void SetStack(UCommonActivatableWidgetStack* Stack);
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Navigation Button")
+	void SetOtherButtons(const TArray<UCommonButtonBase*>& OtherButtons);
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Navigation Button")
+	void SetWidget(TSubclassOf<class UCommonActivatableWidget> WidgetClass);
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Navigation Button")
+	void GetButtonNavigationInfo(UCommonActivatableWidgetStack*& Stack, TSubclassOf<class UCommonActivatableWidget>& WidgetClass);
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Navigation Button")
+	void OnSelected();
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Navigation Button")
+	void UnselectOtherButtons();
+};
