444232536d0bcc4eeeff16f8d8582244bb9c6311
colorindarkness <urielballinas@gmail.com>
2023-04-30 02:56:08 -0700

    Created ElementalInteractionSubsystem

---
+++ Source/TargetVectorCode/Private/Subsystem/ElementalInteractionSubsystem.cpp
@@ -0,0 +1,5 @@
+// MIT
+
+
+#include "Subsystem/ElementalInteractionSubsystem.h"
+
---
+++ Source/TargetVectorCode/Public/Subsystem/ElementalInteractionSubsystem.h
@@ -0,0 +1,17 @@
+// MIT
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "ElementalInteractionSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TARGETVECTORCODE_API UElementalInteractionSubsystem : public UTickableWorldSubsystem
+{
+	GENERATED_BODY()
+	
+};
